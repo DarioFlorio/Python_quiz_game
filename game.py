84650f0c743f4dfdd06b9847f87d9431893ef18a
DarioFlorio <63336471+DarioFlorio@users.noreply.github.com>
2022-08-29 12:05:22 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,10 @@
+# Author:Dario Florio
+# University: University of Sunderland
+# Date:29-08-2022
+###################################################### DEPENDECIES ########################
+# call the game methods from the helpers file 
+from helpers import game
+###################################################### DEPENDECIES ########################
+
+if __name__ == "__main__":
+    game()
